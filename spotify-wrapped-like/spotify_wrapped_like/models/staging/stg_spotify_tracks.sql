5e214aa686261e84533a04b38c681bddeace4fe9
kluczi <bartekcsgo04@gmail.com>
2026-01-24 18:52:13 +0100

    refactor: improve staging model schema and SQL transformations

--- spotify-wrapped-like/spotify_wrapped_like/models/staging/stg_spotify_tracks.sql
+++ spotify-wrapped-like/spotify_wrapped_like/models/staging/stg_spotify_tracks.sql
@@ -6,7 +6,7 @@ with src as (
 )
 
 select 
-    trim(track_id),
+    trim(track_id) as track_id,
     trim(track_name) as track_name,
     track_number::int as track_number,
     track_popularity::int as track_popularity,
@@ -15,10 +15,10 @@ select
     artist_popularity::int as artist_popularity,
     artist_followers::int as artist_followers,
     nullif(trim(artist_genres), 'N/A') as artist_genres,
-    trim(album_id),
+    trim(album_id) as album_id,
     trim(album_name) as album_name,
-    to_date(album_release_date, 'DD-MM-YYYY') as album_release_date,
+    album_release_date::date as album_release_date,
     album_total_tracks::int as album_total_tracks,
-    trim(album_type),
+    trim(album_type) as album_type,
     track_duration_min::double precision as track_duration_min
 from src
