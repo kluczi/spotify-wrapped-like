814c9d3fff370e48259a152717cbb2986111a025
kluczi <bartekcsgo04@gmail.com>
2026-01-24 20:16:04 +0100

    refactor: standardize SQL formatting to uppercase keywords

--- spotify-wrapped-like/spotify_wrapped_like/models/staging/stg_spotify_tracks.sql
+++ spotify-wrapped-like/spotify_wrapped_like/models/staging/stg_spotify_tracks.sql
@@ -1,24 +1,24 @@
-with src as (
-    select 
+WITH src AS (
+    SELECT 
         *
-    from
+    FROM
         {{ref('spotify_data_clean')}}
 )
 
-select 
-    trim(track_id) as track_id,
-    trim(track_name) as track_name,
-    track_number::int as track_number,
-    track_popularity::int as track_popularity,
-    explicit::boolean as explicit,
-    trim(artist_name) as artist_name,
-    artist_popularity::int as artist_popularity,
-    artist_followers::int as artist_followers,
-    nullif(trim(artist_genres), 'N/A') as artist_genres,
-    trim(album_id) as album_id,
-    trim(album_name) as album_name,
-    album_release_date::date as album_release_date,
-    album_total_tracks::int as album_total_tracks,
-    trim(album_type) as album_type,
-    track_duration_min::double precision as track_duration_min
-from src
+SELECT 
+    trim(track_id) AS track_id,
+    trim(track_name) AS track_name,
+    track_number::int AS track_number,
+    track_popularity::int AS track_popularity,
+    explicit::boolean AS explicit,
+    trim(artist_name) AS artist_name,
+    artist_popularity::int AS artist_popularity,
+    artist_followers::int AS artist_followers,
+    nullif(trim(artist_genres), 'N/A') AS artist_genres,
+    trim(album_id) AS album_id,
+    trim(album_name) AS album_name,
+    album_release_date::date AS album_release_date,
+    album_total_tracks::int AS album_total_tracks,
+    trim(album_type) AS album_type,
+    track_duration_min::double precision AS track_duration_min
+FROM src
