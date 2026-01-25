f303b72d2b6f9e24fefae0fd49430dbbee2bee95
kluczi <bartekcsgo04@gmail.com>
2026-01-25 13:38:21 +0100

    feat: add explicit column to dim_track model

--- spotify-wrapped-like/spotify_wrapped_like/models/marts/spotify/dim/dim_track.sql
+++ spotify-wrapped-like/spotify_wrapped_like/models/marts/spotify/dim/dim_track.sql
@@ -4,7 +4,8 @@ WITH src AS (
         track_name,
         track_number,
         track_popularity,
-        track_duration_min
+        track_duration_min,
+        explicit
     FROM 
         {{ref('stg_spotify_tracks')}}
 )
@@ -14,6 +15,7 @@ SELECT
     track_name,
     track_number,
     track_popularity,
-    track_duration_min
+    track_duration_min,
+    explicit
 FROM    
     src
