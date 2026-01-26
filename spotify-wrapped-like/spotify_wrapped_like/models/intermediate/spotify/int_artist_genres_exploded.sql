8e44f46acdc41a6df3d7f65185d5495c81f019cf
kluczi <bartekcsgo04@gmail.com>
2026-01-26 15:32:54 +0100

    feat: add intermediate model to explode artist genres

---
+++ spotify-wrapped-like/spotify_wrapped_like/models/intermediate/spotify/int_artist_genres_exploded.sql
@@ -0,0 +1,14 @@
+WITH src AS (
+    SELECT
+        trim(artist_name) AS artist_name,
+        artist_genres
+    FROM {{ ref('stg_spotify_tracks') }}
+    WHERE artist_genres IS NOT NULL
+)
+
+SELECT DISTINCT
+    artist_name,
+    trim(g) AS genre
+FROM src
+CROSS JOIN unnest(string_to_array(artist_genres, ',')) AS g
+WHERE trim(g) <> ''
