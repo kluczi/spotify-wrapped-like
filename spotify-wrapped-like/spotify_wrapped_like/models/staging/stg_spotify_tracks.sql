5c047ecbeeac1b565fd0e562d6bc50dc7b7e67be
kluczi <bartekcsgo04@gmail.com>
2026-01-30 16:04:32 +0100

    refactor: use normalize_str macro in staging model

--- spotify-wrapped-like/spotify_wrapped_like/models/staging/stg_spotify_tracks.sql
+++ spotify-wrapped-like/spotify_wrapped_like/models/staging/stg_spotify_tracks.sql
@@ -6,19 +6,19 @@ WITH src AS (
 )
 
 SELECT 
-    trim(track_id) AS track_id,
-    trim(track_name) AS track_name,
+    {{ normalize_str('track_id') }} AS track_id,
+    {{ normalize_str('track_name') }} AS track_name,
     track_number::int AS track_number,
     track_popularity::int AS track_popularity,
     explicit::boolean AS explicit,
-    trim(artist_name) AS artist_name,
+    {{ normalize_str('artist_name') }} AS artist_name,
     artist_popularity::int AS artist_popularity,
     artist_followers::int AS artist_followers,
-    nullif(trim(artist_genres), 'N/A') AS artist_genres,
-    trim(album_id) AS album_id,
-    trim(album_name) AS album_name,
+    nullif({{ normalize_str('artist_genres') }}, 'n/a') AS artist_genres,
+    {{ normalize_str('album_id') }} AS album_id,
+    {{ normalize_str('album_name') }} AS album_name,
     album_release_date::date AS album_release_date,
     album_total_tracks::int AS album_total_tracks,
-    trim(album_type) AS album_type,
+    {{ normalize_str('album_type') }} AS album_type,
     track_duration_min::float AS track_duration_min
 FROM src
