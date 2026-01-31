bd45dcc8c30775e9d3cbfbff2bd292438a508690
kluczi <bartekcsgo04@gmail.com>
2026-01-31 11:42:16 +0100

    refactor: use normalize_str macro in intermediate model

--- spotify-wrapped-like/spotify_wrapped_like/models/intermediate/spotify/int_artist_genre_exploded.sql
+++ spotify-wrapped-like/spotify_wrapped_like/models/intermediate/spotify/int_artist_genre_exploded.sql
@@ -1,14 +1,14 @@
 WITH src AS (
     SELECT
-        trim(artist_name) AS artist_name,
+        {{ normalize_str('artist_name') }} AS artist_name,
         artist_genres
     FROM {{ ref('stg_spotify_tracks') }}
     WHERE artist_genres IS NOT NULL
 )
 
 SELECT DISTINCT
-    trim(artist_name) as artist_name,
-    trim(g) AS genre
+    {{ normalize_str('artist_name') }} AS artist_name,
+    {{ normalize_str('g') }} AS genre
 FROM src
 CROSS JOIN unnest(string_to_array(artist_genres, ',')) AS g
-WHERE trim(g) <> ''
+WHERE {{ normalize_str('g') }} <> ''
